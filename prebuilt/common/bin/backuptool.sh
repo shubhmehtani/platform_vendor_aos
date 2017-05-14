DirtyUnicorns/android_vendor_du

Restore custom DPI density after updates
Change-Id: I8d4351a34554baa6f34f97aa44d09cd40ca0095c

prebuilt/common/bin/backuptool.sh
@@ -5,6 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
+export LCDDENSITY="ro.sf.lcd_density"
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
@@ -31,6 +32,7 @@ done
 
 case "$1" in
   backup)
+    grep $LCDDENSITY /system/build.prop > /tmp/du_dpi
     mkdir -p $C
     preserve_addon_d
     run_stage pre-backup
@@ -38,6 +40,14 @@ case "$1" in
     run_stage post-backup
   ;;
   restore)
+    DUDPI=`cat /tmp/du_dpi`
+    if [ "${DUDPI/$LCDDENSITY}" != "$DUDPI" ]
+    then
+        mv /system/build.prop /system/build.prop.new
+        sed "s/ro\.sf\.lcd_density=.*/$DUDPI/g" /system/build.prop.new > /system/build.prop
+        chmod 644 /system/build.prop
+        rm -f /system/build.prop.new
+    fi
     run_stage pre-restore
     run_stage restore
     run_stage post-restore

shubhmehtani/platform_vendor_aos

Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

prebuilt/common/bin/backuptool.sh
@@ -5,6 +5,7 @@
 
 export C=/tmp/backupdir
 export S=/system
+export LCDDENSITY="ro.sf.lcd_density"
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
@@ -31,6 +32,7 @@ done
 
 case "$1" in
   backup)
+    grep $LCDDENSITY /system/build.prop > /tmp/aos_dpi
     mkdir -p $C
     preserve_addon_d
     run_stage pre-backup
@@ -38,6 +40,14 @@ case "$1" in
     run_stage post-backup
   ;;
   restore)
+    AOSDPI=`cat /tmp/aos_dpi`
+    if [ "${AOSDPI/$LCDDENSITY}" != "$AOSDPI" ]
+    then
+        mv /system/build.prop /system/build.prop.new
+        sed "s/ro\.sf\.lcd_density=.*/$AOSDPI/g" /system/build.prop.new > /system/build.prop
+        chmod 644 /system/build.prop
+        rm -f /system/build.prop.new
+    fi
     run_stage pre-restore
     run_stage restore
     run_stage post-restore