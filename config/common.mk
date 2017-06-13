a4345745a1a9941373fb55572d47486965369731
CheckYourScreen <nimitmehta95@gmail.com>
2017-06-13 14:00:27 +0200

    common: use non debug dexpreopter for all devices
    * enables various optimizations included in ART
    
    Signed-off-by: CheckYourScreen <nimitmehta95@gmail.com>
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -149,6 +149,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.dirtyunicorns.utils
 
+# Recommend using the non debug dexpreopter
+USE_DEX2OAT_DEBUG ?= false
 
 # Magisk
 ifeq ($(WITH_ROOT),true)
