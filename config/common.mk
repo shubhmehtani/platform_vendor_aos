289b1606195a5c44220386818df74f146c7ec461
AKabhishek <abhishekkaushik.star@gmail.com>
2017-07-04 08:53:07 +0000

    Switch to Atomic-OS bootanimation
    Thanks @wizper99
    
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -23,9 +23,7 @@ PRODUCT_COPY_FILES += \
     vendor/aos/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
 
 # Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/aos/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
-
+$(call inherit-product, vendor/aos/prebuilt/common/bootanimation/bootanimation.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/aos/overlay/common \
