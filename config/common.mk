DotOS/android_vendor_dot

init: remove sysinit + small cleanup

config/common.mk
@@ -93,8 +93,7 @@ endif
     
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosp/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/aosp/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # JDC-specific init file
 PRODUCT_COPY_FILES += \

shubhmehtani/platform_vendor_aos

Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

config/common.mk
@@ -86,8 +86,7 @@ endif
     
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/aos/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aos/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/aos/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner
 
 # AOS-specific init file
 PRODUCT_COPY_FILES += \