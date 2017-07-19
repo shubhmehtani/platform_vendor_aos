ed8cbdccad1c708cb0add69bc886e58b0a10313f
AKabhishek <abhishekkaushik.star@gmail.com>
2017-07-19 17:28:07 +0000

    script: update with the new exports
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- tools/build_aos.sh
+++ tools/build_aos.sh
@@ -4,8 +4,27 @@
     txtrst=$(tput sgr0)             # reset
     normal='tput sgr0'
 echo -e "";
-echo -e ${red}" Building SUPPORTED variant "${txtrst}
-export AOS_BUILD_TYPE=SUPPORTED
+echo -e "";
+echo -e "";
+echo -e 1- atomicity
+echo -e "";
+echo -e 2- weekly
 echo -e "";
 echo -e "";
 $normal
+read askvariant
+if [ "$askvariant" == "1" ]
+then echo -e "";
+echo -e ▪Building atomicity
+export AOS_BUILD_TYPE=atomicity
+echo -e "";
+echo -e "";
+fi
+if [ "$askvariant" == "2" ]
+then
+echo -e "";
+$normal
+echo -e ▪ Building weekly
+export AOS_BUILD_TYPE=weekly
+echo -e "";
+fi
