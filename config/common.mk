0a205a8326e71ca4fae63ff3f74c7db14a1710a1
bigrushdog <randall.rushing@gmail.com>
2017-10-28 16:43:00 -0700

    DUI: Initial checkin for Oreo [2/7]
    Change-Id: Ieca38bd1cf92e4d75d65a22657b4c8d30cd41f6e
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -102,12 +102,12 @@ PRODUCT_PACKAGES += \
 #endif
 
 # DU Utils library
-#PRODUCT_BOOT_JARS += \
-#    org.dirtyunicorns.utils
+PRODUCT_BOOT_JARS += \
+    org.dirtyunicorns.utils
 
 # DU Utils library
-#PRODUCT_PACKAGES += \
-#    org.dirtyunicorns.utils
+PRODUCT_PACKAGES += \
+    org.dirtyunicorns.utils
 
 #MusicFX
 ifeq ($(WITH_MUSICFX),true)
